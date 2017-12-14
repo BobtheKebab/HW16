879f7036a98c0d219bd9e0d99169a593f5650f54
ykadir42 <ykadir42@gmail.com>
2017-12-14 00:36:09 -0500

    server handshake

--- pipe_networking.c
+++ pipe_networking.c
@@ -11,7 +11,18 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
-  return 0;
+  if(mkfifo("WKP", 0644)<0) printf("%s\n", strerror(errno));
+  int srv_pipe = open("WKP", O_RDONLY);
+  char name[25];
+  printf("About to read\n");
+  read(srv_pipe, name, sizeof(name));
+  printf("%s\n", name);
+  close(srv_pipe);
+  remove("WKP");
+  int clt_pipe = open(name, O_WRONLY);
+  write(clt_pipe, name, sizeof(name));
+  *to_client = clt_pipe;
+  return clt_pipe;
 }
 
 
@@ -37,9 +48,8 @@ int client_handshake(int *to_server)  {
   char temp[25];
   read(clt_pipe, temp, sizeof(temp));
   if (!strcmp(name, temp)) {
-    close(srv_pipe);
+	*to_server = srv_pipe;
+	return clt_pipe;
   }
-  
-  return 0;
-  
+
 }
